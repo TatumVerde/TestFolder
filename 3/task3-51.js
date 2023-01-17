5d6187a75988bd5a9cce614f94acd246f04c12ef
Vlad <jaker8789@gmail.com>
2023-01-17 09:19:35 +0200

    review task3-51 - Boolean 23

--- 3/task3-51.js
+++ 3/task3-51.js
@@ -1,3 +1,7 @@
+/* Boolean 23
+Дано четырехзначное число. Проверить истинность высказывания: «Данное число 
+читается одинаково слева направо и справа налево». */
+
 let A = parseInt(prompt('Введіть чотирьохзначне число'))
 
 let thousands = Math.floor(A / 1000)
@@ -9,5 +13,10 @@ let tens = Math.floor(A % 100 / 10)
 let single = A % 10
 
 let result = thousands == single && tens == hundreds
+// є ще другий варіант який я показував
+// let compare = single * 1000 + tens * 100 + hundreds * 10 + thousands
+// let result = A == compare
 
 alert(result)
+
+// Правильнно, але це я тобі виправив, ти щось намудрила з порівнянніми)
