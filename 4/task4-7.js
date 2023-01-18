04d82e50672ced2e4aa5a15bc8b43b643241f3de
TatumVerde <slobodenuktetianka@gmail.com>
2023-01-18 17:01:28 +0200

    change in task4-7.js

--- 4/task4-7.js
+++ 4/task4-7.js
@@ -4,8 +4,10 @@ let x1 = parseInt(prompt(massege + 'x1'))
 
 let x2 = parseInt(prompt(massege + 'x2'))
 
-if (x1 > x2) {
+if (x1 < x2) {
     alert(1)
-} else {
+} else if (x2 < x1){
     alert(2)
+} else {
+    alert("1 і 2 - рівні")
 }
