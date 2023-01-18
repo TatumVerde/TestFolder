7c746b36210f93486eec0c8ae248149f6be3caa8
TatumVerde <slobodenuktetianka@gmail.com>
2023-01-18 10:47:14 +0200

    task4-4

---
+++ 4/task4-4.js
@@ -0,0 +1,23 @@
+let massege = 'Введіть число'
+
+let x1 = parseInt(prompt(massege + ' x1'))
+
+let x2 = parseInt(prompt(massege + ' x2'))
+
+let x3 = parseInt(prompt(massege + ' x3'))
+
+let n = 0
+
+if (x1 > 0) {
+    n = n + 1 
+}
+
+if (x2 > 0) {
+    n = n + 1
+}
+
+if (x3 > 0) {
+    n = n + 1
+}
+   
+alert(n)
