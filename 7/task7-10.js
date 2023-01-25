5495382243aaa91eeef09fc667c619b57755ceac
TatumVerde <slobodenuktetianka@gmail.com>
2023-01-25 10:55:12 +0200

    task7.6-11

---
+++ 7/task7-10.js
@@ -0,0 +1,8 @@
+let n = parseInt(prompt('Введіть кількість разів'));
+
+let i = 0;
+
+while (i < n) {
+    console.log(7)
+    i++
+}
---
+++ 7/task7-11.js
@@ -0,0 +1,8 @@
+let k = parseInt(prompt('Введіть число'));
+
+let i = 0;
+
+while (i < 12) {
+    console.log(k);
+    i++
+}
