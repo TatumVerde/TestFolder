23fa7edfdeab89c1545a63723ac5ff96ffef597f
TatumVerde <slobodenuktetianka@gmail.com>
2023-02-06 14:33:37 +0200

    ee

--- 11/task11-34.js
+++ 11/task11-34.js
@@ -2,10 +2,22 @@ let n = parseInt(prompt('Введіть число'));
 
 let arr = new Array(n)
 
-let str = ''
+let str1 = ''
+
+let str2 = ''
 
 for (i = 0; i < arr.length; i++) {
     arr[i] = Math.floor(Math.random() * 2)
-    str = str + arr[i]
+    str1 = str1 + arr[i]
+    for(j = str1.length; j >= 0; j--)
+    if (j == 0) {
+        j == 1
+    } else if (j == 1) {
+        j == 0
+    }
+    str2 = str2 + 
+  
 }
-alert(str)
+
+
+alert(str1 + ' ' + str2)
